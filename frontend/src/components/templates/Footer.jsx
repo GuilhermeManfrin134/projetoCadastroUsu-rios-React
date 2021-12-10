00f075cf60e45a07b99dbf110734bf2f63812d9d
GuiManfrin134 <manfrin11gui@gmail.com>
2021-12-10 17:34:00 -0300

    Adicionando LinkedIn no Footer

--- frontend/src/components/templates/Footer.jsx
+++ frontend/src/components/templates/Footer.jsx
@@ -4,8 +4,8 @@ import './Footer.css'
 
 export default props =>
     <footer className="footer">
-        <span>
-            Desenvolvido com <i className="fa fa-heart text-danger"></i> por <strong>Guilherme Manfrin Pereira</strong>
+        <span>Desenvolvido com <i className="fa fa-heart text-danger"></i> por
+            <strong><a target="_blank" href="https://www.linkedin.com/in/guilherme-manfrin-pereira-4875a2207/"> Guilherme Manfrin Pereira</a></strong>
         </span>
             
     </footer>
